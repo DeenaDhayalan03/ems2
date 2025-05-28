85850bc89c76ae4f3e108e6c73215811aa22eae1
DeenadhayalanM <deenadhayalan.m@corp.knowledgelens.com>
2025-05-28 12:57:09 +0530

    changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
 from scripts.services.service import router
 
-app = FastAPI(root_path="/gateway/plugin/project-139/ems-plug/api")
+app = FastAPI(root_path="/gateway/plugin/project-139/api-ems/api")
 app.include_router(router)
